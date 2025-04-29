2f79b5d5a94e50a664aa87476cb128a05e4a5a36
Jakehebert201 <hebertj@etsu.edu>
2025-04-29 15:53:43 -0400

    User input, GUI, Plot and Table fully implemented

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,9 +37,6 @@ partial class Form1
         labelMutationRate = new Label();
         labelShowPopSize = new Label();
         labelPopSize = new Label();
-        dataGridView1 = new DataGridView();
-        Individual = new DataGridViewTextBoxColumn();
-        Weight = new DataGridViewTextBoxColumn();
         userInputFunc = new TextBox();
         btnReset = new Button();
         labelFunctionInfo = new Label();
@@ -49,10 +46,9 @@ partial class Form1
         labelMinRange = new Label();
         labelMaxRange = new Label();
         numMinRange = new NumericUpDown();
-        formsPlot1 = new ScottPlot.WinForms.FormsPlot();
+        tabControl1 = new TabControl();
         ((System.ComponentModel.ISupportInitialize)numPopSize).BeginInit();
         ((System.ComponentModel.ISupportInitialize)numMutationRate).BeginInit();
-        ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
         ((System.ComponentModel.ISupportInitialize)numGenerations).BeginInit();
         ((System.ComponentModel.ISupportInitialize)numMaxRange).BeginInit();
         ((System.ComponentModel.ISupportInitialize)numMinRange).BeginInit();
@@ -65,7 +61,7 @@ partial class Form1
         btnRun.Location = new Point(335, 202);
         btnRun.Name = "btnRun";
         btnRun.Size = new Size(330, 35);
-        btnRun.TabIndex = 0;
+        btnRun.TabIndex = 8;
         btnRun.Text = "Run Genetic Algorithm";
         btnRun.UseVisualStyleBackColor = false;
         btnRun.Click += btnRun_Click;
@@ -77,7 +73,7 @@ partial class Form1
         chkMinimize.Location = new Point(573, 66);
         chkMinimize.Name = "chkMinimize";
         chkMinimize.Size = new Size(80, 19);
-        chkMinimize.TabIndex = 3;
+        chkMinimize.TabIndex = 5;
         chkMinimize.Text = "Minimize?";
         chkMinimize.UseVisualStyleBackColor = false;
         chkMinimize.CheckedChanged += chkMinimize_CheckedChanged;
@@ -88,7 +84,7 @@ partial class Form1
         numPopSize.Location = new Point(376, 65);
         numPopSize.Name = "numPopSize";
         numPopSize.Size = new Size(44, 23);
-        numPopSize.TabIndex = 4;
+        numPopSize.TabIndex = 3;
         // 
         // numMutationRate
         // 
@@ -99,7 +95,7 @@ partial class Form1
         numMutationRate.Maximum = new decimal(new int[] { 1, 0, 0, 0 });
         numMutationRate.Name = "numMutationRate";
         numMutationRate.Size = new Size(44, 23);
-        numMutationRate.TabIndex = 5;
+        numMutationRate.TabIndex = 4;
         // 
         // LabelSettingsSummary
         // 
@@ -109,7 +105,7 @@ partial class Form1
         LabelSettingsSummary.Location = new Point(460, 240);
         LabelSettingsSummary.Name = "LabelSettingsSummary";
         LabelSettingsSummary.Size = new Size(69, 15);
-        LabelSettingsSummary.TabIndex = 6;
+        LabelSettingsSummary.TabIndex = 100;
         LabelSettingsSummary.Text = "placeholder";
         LabelSettingsSummary.Visible = false;
         LabelSettingsSummary.Click += LabelSettingsSummary_Click;
@@ -121,7 +117,7 @@ partial class Form1
         labelMutationRate.Location = new Point(460, 102);
         labelMutationRate.Name = "labelMutationRate";
         labelMutationRate.Size = new Size(82, 15);
-        labelMutationRate.TabIndex = 7;
+        labelMutationRate.TabIndex = 100;
         labelMutationRate.Text = "Mutation Rate";
         // 
         // labelShowPopSize
@@ -139,29 +135,8 @@ partial class Form1
         labelPopSize.Location = new Point(351, 102);
         labelPopSize.Name = "labelPopSize";
         labelPopSize.Size = new Size(88, 15);
-        labelPopSize.TabIndex = 9;
+        labelPopSize.TabIndex = 100;
         labelPopSize.Text = "Population Size";
-        // 
-        // dataGridView1
-        // 
-        dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-        dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Individual, Weight });
-        dataGridView1.Location = new Point(376, 333);
-        dataGridView1.Name = "dataGridView1";
-        dataGridView1.Size = new Size(250, 250);
-        dataGridView1.TabIndex = 10;
-        dataGridView1.Visible = false;
-        dataGridView1.CellContentClick += dataGridView1_CellContentClick;
-        // 
-        // Individual
-        // 
-        Individual.HeaderText = "Individual";
-        Individual.Name = "Individual";
-        // 
-        // Weight
-        // 
-        Weight.HeaderText = "Weight";
-        Weight.Name = "Weight";
         // 
         // userInputFunc
         // 
@@ -169,7 +144,7 @@ partial class Form1
         userInputFunc.Multiline = true;
         userInputFunc.Name = "userInputFunc";
         userInputFunc.Size = new Size(330, 34);
-        userInputFunc.TabIndex = 11;
+        userInputFunc.TabIndex = 7;
         userInputFunc.TextChanged += userInputFunc_TextChanged;
         // 
         // btnReset
@@ -177,7 +152,7 @@ partial class Form1
         btnReset.Location = new Point(671, 202);
         btnReset.Name = "btnReset";
         btnReset.Size = new Size(97, 33);
-        btnReset.TabIndex = 12;
+        btnReset.TabIndex = 9;
         btnReset.Text = "Reset?";
         btnReset.UseVisualStyleBackColor = true;
         // 
@@ -188,7 +163,7 @@ partial class Form1
         labelFunctionInfo.Location = new Point(419, 170);
         labelFunctionInfo.Name = "labelFunctionInfo";
         labelFunctionInfo.Size = new Size(159, 15);
-        labelFunctionInfo.TabIndex = 13;
+        labelFunctionInfo.TabIndex = 100;
         labelFunctionInfo.Text = "Create a function. ex. (x^2-9)";
         // 
         // numGenerations
@@ -197,7 +172,7 @@ partial class Form1
         numGenerations.Maximum = new decimal(new int[] { 1000000, 0, 0, 0 });
         numGenerations.Name = "numGenerations";
         numGenerations.Size = new Size(84, 23);
-        numGenerations.TabIndex = 14;
+        numGenerations.TabIndex = 6;
         numGenerations.Value = new decimal(new int[] { 1000, 0, 0, 0 });
         // 
         // labelGenerationCount
@@ -207,7 +182,7 @@ partial class Form1
         labelGenerationCount.Location = new Point(684, 102);
         labelGenerationCount.Name = "labelGenerationCount";
         labelGenerationCount.Size = new Size(92, 15);
-        labelGenerationCount.TabIndex = 15;
+        labelGenerationCount.TabIndex = 100;
         labelGenerationCount.Text = "No. Generations";
         // 
         // numMaxRange
@@ -215,7 +190,7 @@ partial class Form1
         numMaxRange.Location = new Point(275, 64);
         numMaxRange.Name = "numMaxRange";
         numMaxRange.Size = new Size(41, 23);
-        numMaxRange.TabIndex = 16;
+        numMaxRange.TabIndex = 2;
         // 
         // labelMinRange
         // 
@@ -224,7 +199,7 @@ partial class Form1
         labelMinRange.Location = new Point(129, 102);
         labelMinRange.Name = "labelMinRange";
         labelMinRange.Size = new Size(96, 15);
-        labelMinRange.TabIndex = 17;
+        labelMinRange.TabIndex = 100;
         labelMinRange.Text = "Minimum Range";
         // 
         // labelMaxRange
@@ -234,7 +209,7 @@ partial class Form1
         labelMaxRange.Location = new Point(245, 102);
         labelMaxRange.Name = "labelMaxRange";
         labelMaxRange.Size = new Size(97, 15);
-        labelMaxRange.TabIndex = 18;
+        labelMaxRange.TabIndex = 100;
         labelMaxRange.Text = "Maximum Range";
         // 
         // numMinRange
@@ -243,23 +218,24 @@ partial class Form1
         numMinRange.Minimum = new decimal(new int[] { 100, 0, 0, int.MinValue });
         numMinRange.Name = "numMinRange";
         numMinRange.Size = new Size(41, 23);
-        numMinRange.TabIndex = 19;
+        numMinRange.TabIndex = 1;
         // 
-        // formsPlot1
+        // tabControl1
         // 
-        formsPlot1.DisplayScale = 1F;
-        formsPlot1.Location = new Point(671, 324);
-        formsPlot1.Name = "formsPlot1";
-        formsPlot1.Size = new Size(489, 331);
-        formsPlot1.TabIndex = 20;
+        tabControl1.Location = new Point(65, 301);
+        tabControl1.Name = "tabControl1";
+        tabControl1.SelectedIndex = 0;
+        tabControl1.Size = new Size(966, 436);
+        tabControl1.TabIndex = 21;
+        tabControl1.Visible = false;
         // 
         // Form1
         // 
         AutoScaleMode = AutoScaleMode.Inherit;
         AutoSize = true;
         BackColor = SystemColors.ActiveCaption;
-        ClientSize = new Size(1498, 811);
-        Controls.Add(formsPlot1);
+        ClientSize = new Size(1129, 811);
+        Controls.Add(tabControl1);
         Controls.Add(numMinRange);
         Controls.Add(labelMaxRange);
         Controls.Add(labelMinRange);
@@ -269,7 +245,6 @@ partial class Form1
         Controls.Add(labelFunctionInfo);
         Controls.Add(btnReset);
         Controls.Add(userInputFunc);
-        Controls.Add(dataGridView1);
         Controls.Add(labelPopSize);
         Controls.Add(labelShowPopSize);
         Controls.Add(labelMutationRate);
@@ -283,7 +258,6 @@ partial class Form1
         Load += Form1_Load;
         ((System.ComponentModel.ISupportInitialize)numPopSize).EndInit();
         ((System.ComponentModel.ISupportInitialize)numMutationRate).EndInit();
-        ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
         ((System.ComponentModel.ISupportInitialize)numGenerations).EndInit();
         ((System.ComponentModel.ISupportInitialize)numMaxRange).EndInit();
         ((System.ComponentModel.ISupportInitialize)numMinRange).EndInit();
@@ -311,9 +285,6 @@ partial class Form1
     private Label labelMutationRate;
     private Label labelShowPopSize;
     private Label labelPopSize;
-    private DataGridView dataGridView1;
-    private DataGridViewTextBoxColumn Individual;
-    private DataGridViewTextBoxColumn Weight;
     private TextBox userInputFunc;
     private Button btnReset;
     private Label labelFunctionInfo;
@@ -323,5 +294,5 @@ partial class Form1
     private Label labelMinRange;
     private Label labelMaxRange;
     private NumericUpDown numMinRange;
-    private ScottPlot.WinForms.FormsPlot formsPlot1;
+    private TabControl tabControl1;
 }
--- Form1.cs
+++ Form1.cs
@@ -6,6 +6,7 @@ using System.Security.AccessControl;
 using System.Linq.Expressions;
 using static GradientDescent.GradientDescent;
 using Antlr4.Runtime.Misc;
+using ScottPlot;
 
 public partial class Form1 : Form
 {
@@ -25,6 +26,7 @@ public partial class Form1 : Form
     private double maxRange;
     private GradientDescent.OptimizationFunction func;
     private string functionText;
+    private int printInterval => Math.Max(1,generations / 10); //Interval to generate table and plot across the generations
     private void Form1_Load(object sender, EventArgs e)
     {
 
@@ -32,6 +34,7 @@ public partial class Form1 : Form
 
     private void btnRun_Click(object sender, EventArgs e)
     {
+        tabControl1.TabPages.Clear();
         //Vals from UI
         populationSize = int.Parse(numPopSize.Text);
         MutationRate = double.Parse(numMutationRate.Text);
@@ -43,7 +46,7 @@ public partial class Form1 : Form
         //Settings summary label
         LabelSettingsSummary.Visible = true;
         LabelSettingsSummary.Text = $"Population Size: {populationSize}\nMutation Rate: {MutationRate}\nMinimized? {minimize}";
-        
+
         //Function input
         func = GetUserDefinedFunction();
         if (func == null) { return; }
@@ -53,32 +56,19 @@ public partial class Form1 : Form
         for (int i = 0; i < generations; i++)
         {
             ga.Evolve();
-        }
-        double[] Population = ga.GetPopulation();
-        //Plot
-        double[] xs = Population;
-        double[] ys = xs.Select(x => func(x)).ToArray();
-            //Sort values
-        var sorted = xs.Zip(ys, (x, y) => new { x, y }).OrderBy(Pair => Pair.x).ToArray();
+            //Take a snapshot based on interval
+            if (i % printInterval == 0 || i == generations-1)
+            {
+                double[] Population = ga.GetPopulation();
+                int labelgen = (i == generations - 1) ? generations : i;
+                SaveSnapshot(labelgen, Population);
 
-        double[] sortedXs = sorted.Select(p => p.x).ToArray();
-        double[] sortedYs = sorted.Select(p => p.y).ToArray();
-        formsPlot1.Plot.Clear();
-        formsPlot1.Plot.Add.Scatter(sortedXs, sortedYs);
-        formsPlot1.Plot.Title($"Final Population after {generations}");
-        formsPlot1.Plot.XLabel("x");
-        formsPlot1.Plot.YLabel($"f(x) = {functionText}");
-        formsPlot1.Refresh();
+            }
+            }
 
-
-        //populate table
-        foreach (var individual in Population)
-        {
-            double fx = func(individual);
-            dataGridView1.Rows.Add(individual.ToString("F4"), fx.ToString("F4"));
-        }
-        dataGridView1.Visible = true;
     }
+
+        
     private void btnReset_Click(object sender, EventArgs e)
     {
         //Reread UI values
@@ -95,9 +85,6 @@ public partial class Form1 : Form
         //Reset Ga
         ga = new(populationSize, MutationRate, func, minimize, minRange, maxRange);
 
-        //Clear table
-        dataGridView1.Visible = false;
-        dataGridView1.Rows.Clear();
 
     }
     public OptimizationFunction GetUserDefinedFunction()
@@ -115,6 +102,55 @@ public partial class Form1 : Form
             MessageBox.Show($"Invalid function: {ex.Message}");
             return null; // or throw if you want stricter control
         }
+    }
+    private void Drawplot(ScottPlot.WinForms.FormsPlot plot, double[] pop)
+    {
+        //Plot
+        double[] ys = pop.Select(x => func(x)).ToArray();
+        //Sort values
+        var sorted = pop.Zip(ys, (x, y) => new { x, y }).OrderBy(Pair => Pair.x).ToArray();
+
+        double[] sortedXs = sorted.Select(p => p.x).ToArray();
+        double[] sortedYs = sorted.Select(p => p.y).ToArray();
+
+        plot.Plot.Clear();
+        plot.Plot.Add.Scatter(sortedXs, sortedYs);
+        plot.Plot.Title($"Population after {generations}");
+        plot.Plot.XLabel("x");
+        plot.Plot.YLabel($"f(x) = {functionText}");
+        plot.Refresh();
+    }
+    private void DrawTable(DataGridView grid, double[] pop)
+    {
+        grid.Columns.Clear();
+        grid.Rows.Clear();
+
+        grid.Columns.Add("X", "Individual");
+        grid.Columns.Add("Y", "Fitness");
+
+        //populate table
+        foreach (var individual in pop)
+        {
+            grid.Rows.Add(individual.ToString("F4"), func(individual).ToString("F4"));
+        }
+        
+    }
+    private void SaveSnapshot(int generation, double[] population)
+    {
+        var tabPage = new TabPage($"Gen {generation:N0}");
+        var grid = new DataGridView() { Dock = DockStyle.Top, Height = 200 };
+        var formsPlot = new ScottPlot.WinForms.FormsPlot() { Dock = DockStyle.Fill};
+
+        
+
+        DrawTable(grid, population);
+        Drawplot(formsPlot, population);
+
+        tabPage.Controls.Add(grid);
+        tabPage.Controls.Add(formsPlot);
+        tabControl1.TabPages.Add(tabPage);
+        tabControl1.Visible = true;
+        
     }
 
     private void textBox1_TextChanged(object sender, EventArgs e)
